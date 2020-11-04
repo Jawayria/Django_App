27f2b70ca311dd3bbd03a29cf15b27a3e2db604f
Jawayria Hashmi <jawayriahashmi@gmail.com>
2020-11-04 14:48:39 +0500

    Completed the celery integration to insert the data

--- cricPredict/Contest/tasks.py
+++ cricPredict/Contest/tasks.py
@@ -4,43 +4,56 @@ import requests
 import json
 from .models import League, Match
 from datetime import datetime
+from django.utils.timezone import get_current_timezone
 
 
 @periodic_task(run_every=(crontab(minute='*')), name="fetch_data", ignore_result=True)
 def fetch_data():
-    url = "https://rapidapi.p.rapidapi.com/series.php"
+    try:
+        url = "https://rapidapi.p.rapidapi.com/series.php"
 
-    headers = {
-        'x-rapidapi-key': "94d65a12cbmsh65295dae3718214p1ee3b8jsn335c8c3f26ef",
-        'x-rapidapi-host': "dev132-cricket-live-scores-v1.p.rapidapi.com"
-    }
+        headers = {
+            'x-rapidapi-key': "94d65a12cbmsh65295dae3718214p1ee3b8jsn335c8c3f26ef",
+            'x-rapidapi-host': "dev132-cricket-live-scores-v1.p.rapidapi.com"
+        }
 
-    response = requests.get(url, headers=headers)
-    response = json.loads(response.text)
+        response = requests.get(url, headers=headers)
+        response = json.loads(response.text)
 
-    for series in response['seriesList']['series']:
+        for series in response['seriesList']['series']:
 
-        start_date = series['startDateTime'].split('/')
-        start_date = datetime(int(start_date[2]), int(start_date[0]), int(start_date[1]))
+            start_date = series['startDateTime'].split('/')
+            start_date = datetime(int(start_date[2]), int(start_date[0]), int(start_date[1]))
 
-        if start_date > datetime.now():
+            if start_date > datetime.now():
 
-            url = "https://rapidapi.p.rapidapi.com/seriesteams.php"
-            teams = requests.get(url, headers=headers, params={"seriesid":series["id"]})
-            teams = json.loads(teams.text)
+                url = "https://rapidapi.p.rapidapi.com/seriesteams.php"
+                teams = requests.get(url, headers=headers, params={"seriesid":series["id"]})
+                teams = json.loads(teams.text)
 
-            if len(teams['seriesTeams']['teams']) >= 2:
-                end_date = series['endDateTime'].split('/')
-                end_date = datetime(int(end_date[2]), int(end_date[0]), int(end_date[1]))
+                if len(teams['seriesTeams']['teams']) > 2:
+                    end_date = series['endDateTime'].split('/')
+                    end_date = datetime(int(end_date[2]), int(end_date[0]), int(end_date[1]))
 
-                league = League(name=series['name'], start_date=start_date.strftime("%Y-%m-%d"),
-                                end_date=end_date.strftime("%Y-%m-%d"))
+                    league = League(api_id=series['id'], name=series['name'], start_date=start_date.strftime("%Y-%m-%d"),
+                                    end_date=end_date.strftime("%Y-%m-%d"))
 
-                url = "https://rapidapi.p.rapidapi.com/matchseries.php"
-                matches = requests.get(url, headers=headers, params={"seriesid":series["id"]})
-                matches = json.loads(matches.text)
+                    league.save()
+                    league = League.objects.filter(api_id=series['id'])[0]
 
-                # create match objects and save data in DB
-    return response['seriesList']
+                    url = "https://rapidapi.p.rapidapi.com/matchseries.php"
+                    matches = requests.get(url, headers=headers, params={"seriesid":series["id"]})
+                    matches = json.loads(matches.text)
 
+                    for match in matches['matchList']['matches']:
+                        team1 = match['homeTeam']['name']
+                        team2 = match['awayTeam']['name']
+                        start_time = datetime.strptime(match['startDateTime'], "%Y-%m-%dT%H:%M:%SZ")\
+                            .replace(tzinfo=get_current_timezone())
+                        new_match = Match(league=league, team1=team1, team2=team2, time=start_time);
+                        new_match.save()
+
+        return True
+    except:
+        return
 
