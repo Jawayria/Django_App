008ff02b794552422debafe549e35b31f7e3ba09
Jawayria Hashmi <jawayriahashmi@gmail.com>
2020-11-13 15:26:02 +0500

    Added a test

--- cricPredict/Groups/tests.py
+++ cricPredict/Groups/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from unittest import TestCase
 
 # Create your tests here.
+from pytest import raises
+
+
+def divide(num1, num2):
+    return num1/num2
+
+
+class TestDivideByZero(TestCase):
+    def test_division(self):
+        self.assertRaises(ZeroDivisionError, divide, 2, 0)
